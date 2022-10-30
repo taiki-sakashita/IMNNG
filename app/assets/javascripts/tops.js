1cc67de7b564470c41ae9df4413ad295088e74b8
taiki-sakashita <minashi.business@gmail.com>
2022-10-30 22:25:27 +0900

    kuwata_add,CSS

--- app/assets/javascripts/tops.js
+++ app/assets/javascripts/tops.js
@@ -1,15 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("audio");
   audio.addEventListener('click', function () {
-    document.getElementById("sounds").currentTime = 0;
-    document.getElementById("sounds").play();
-  });
-});
-
-
-window.addEventListener("DOMContentLoaded", () => {
-  const audio = document.getElementById("audio2");
-  audio.addEventListener('click', function () {
     document.getElementById("sounds2").currentTime = 0;
     document.getElementById("sounds2").play();
   });
