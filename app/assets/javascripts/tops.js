868219e786a04840be9441214a68d6787e5af42f
taiki-sakashita <minashi.business@gmail.com>
2023-07-19 17:55:15 +0900

    使っていないであろうファイル、コードの掃除

--- app/assets/javascripts/tops.js
+++ app/assets/javascripts/tops.js
@@ -19,22 +19,3 @@ function loadPartialView(loadPath) {
   $("#button_area").load(loadPath, function (response) {
   });
 }
-
-
-/*function readAloud() {
-  // テキストを取得
-  const text = document.getElementById("ima").value
-
-  // ブラウザにWeb Speech API Speech Synthesis機能があるか判定
-  if ('speechSynthesis' in window) {
-    // 発言を設定
-    const uttr = new SpeechSynthesisUtterance()
-    uttr.text = text
-    // 発言を再生
-    window.speechSynthesis.speak(uttr)
-
-  } else {
-    alert('大変申し訳ありません。このブラウザは音声合成に対応していません。')
-  }
-}
-*/
